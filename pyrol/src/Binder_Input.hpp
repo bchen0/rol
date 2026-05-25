ac02dcfe387467ee2e657900ff9c229e1c4729f1
bchen0 <brichen@sandia.gov>
2026-05-25 12:36:53 -0600

    fix

--- pyrol/src/Binder_Input.hpp
+++ pyrol/src/Binder_Input.hpp
@@ -9,7 +9,7 @@
 
 #include <PyROL_stream.hpp>
 
-#ifndef PYROL_BINDER_PARSING
-#include <PyROL_Teuchos_ETI.hpp>
-#include "PyROL_ETI.hpp"
-#endif
+// #ifndef PYROL_BINDER_PARSING
+// #include <PyROL_Teuchos_ETI.hpp>
+// #include "PyROL_ETI.hpp"
+// #endif
